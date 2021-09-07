d3e9e8a98e3ef93f21f9edbe1e4eab14a10dbed0
c-obol <78653930+c-obol@users.noreply.github.com>
2021-09-07 15:30:54 +0900

    update

--- server/room.py
+++
@@ -1,4 +0,0 @@
-class room():
-    def __init__(self, max_player):
-        self.max_player = max_player
-        self.player_list = []
---
+++ server/room_class.py
@@ -0,0 +1,11 @@
+class room():
+    def __init__(self, max_player):
+        self.min_player = 2
+        self.max_player = max_player
+        self.player_list = []
+
+    def play_game():
+        pass
+
+    def in_game():
+        pass
