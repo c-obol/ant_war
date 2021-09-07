cb99e1d54618b095ae2fbd44752fd1982e1def69
c-obol <78653930+c-obol@users.noreply.github.com>
2021-09-07 16:52:12 +0900

    update

--- client/template/script/main.js
+++ client/template/script/main.js
@@ -1,5 +1,14 @@
 
 
-window.onload(function() {
-
-})
+window.onload = function() {
+    $.getJSON("./data/server_list.json", function(json){
+        for (let i = 0; i < json.lengh; i++) {
+            $("select#server_list")
+            .append(
+                `<option value = "http://${json[i].host}:${json[i].port}">
+                    ${json[i].name}
+                </option>`
+            )
+        }
+    })
+}
